8116b5fd8123aeb947bdf2ee9dece02816de98f6
Dead End <dead.end@inbox.ru>
2024-01-06 13:14:40 +0100

    validators

--- src/ts/validation/validators.tsx
+++ src/ts/validation/validators.tsx
@@ -1,7 +1,38 @@
 import { TValidator } from "./types";
 
-export const requiredValidator: TValidator = ({ value }) => {
+export const requiredValidator: TValidator = (value) => {
   if (!value) {
     return "Please enter a value!";
   }
+};
+
+export const validateFieldEquals = (field: string, msg: string): TValidator => {
+  return (value, form) => {
+    if (value !== form[field]) {
+      return msg;
+    }
+  };
+};
+
+export const validateMinMax = (props: {
+  min?: number;
+  max?: number;
+}): TValidator => {
+  return (value) => {
+    if (typeof value === "string") {
+      if (props.min && value.length < props.min) {
+        return `The minumum length is: ${props.min}`;
+      }
+      if (props.max && value.length > props.max) {
+        return `The maximum length is: ${props.max}`;
+      }
+    } else if (typeof value === "number") {
+      if (props.min && value < props.min) {
+        return `The minumum value is: ${props.min}`;
+      }
+      if (props.max && value > props.max) {
+        return `The maximum value is: ${props.max}`;
+      }
+    }
+  };
 };
